3ca9e97b9b29b4cce63591a1021318ea42322abe
Glendon Chin <glendonchin@gmail.com>
2022-05-30 19:46:59 -0400

    bring entry from database to display + add dates

--- entries/views.py
+++ entries/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Entry
 
 # Create your views here.
 def index(request):
-    return render(request, 'entries/index.html')
+    entries = Entry.objects.order_by('-date')
+
+    context = {'entries': entries}
+    return render(request, 'entries/index.html', context)
 
 def add(request):
     return render(request, 'entries/add.html')
