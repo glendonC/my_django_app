297fbdd4891cb29417c402842271bbc173d131f9
Glendon Chin <glendonchin@gmail.com>
2022-05-30 21:03:08 -0400

    add processing forms (csrf token)

--- entries/views.py
+++ entries/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Entry
 from .forms import EntryForm
 # Create your views here.
@@ -9,6 +9,17 @@ def index(request):
     return render(request, 'entries/index.html', context)
 
 def add(request):
+
+    if request.method == "POST":
+        form=EntryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form=EntryForm()
+        
+
     form = EntryForm()
     context = {'form': form}
     return render(request, 'entries/add.html', context)
